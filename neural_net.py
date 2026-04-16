3b5965b16679cb37b2ee552253a08310a8cfc146
BrownF47 <alexbrown47@virginmedia.com>
2026-04-16 23:43:34 +0100

    back prop

--- neural_net.py
+++ neural_net.py
@@ -22,33 +22,46 @@ class NeuralNet():
         self.biases_2 = np.random.rand(1,12)
         self.biases_3 = np.random.rand(1,6)
         self.biases_4 = np.zeros((1,1))
+
+        self.alpha = 0.0001
         
     def ReLU(self, x):
         return np.maximum(0, x)
+    
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
         
     def propagate_layer_forward(self, weights, biases, previous_layer):
-        #print(np.shape(weights))
-        #print(np.shape(previous_layer))
-        next_layer = self.ReLU(np.matmul(previous_layer, weights) + biases)
+        next_layer = np.matmul(previous_layer, weights) + biases
 
         return next_layer
     
     def propagate_network_forward(self, state):
-        hidden_layer_1 = self.propagate_layer_forward(self.weights_1, self.biases_1, state)
-        hidden_layer_2 = self.propagate_layer_forward(self.weights_2, self.biases_2, hidden_layer_1)
-        hidden_layer_3 = self.propagate_layer_forward(self.weights_3, self.biases_3, hidden_layer_2)
-        output = self.propagate_layer_forward(self.weights_4, self.biases_4, hidden_layer_3)
+        self.layer_1 = self.propagate_layer_forward(self.weights_1, self.biases_1, state)
+        self.layer_2 = self.propagate_layer_forward(self.weights_2, self.biases_2, self.ReLU(self.layer_1))
+        self.layer_3 = self.propagate_layer_forward(self.weights_3, self.biases_3, self.ReLU(self.layer_2))
+        output = self.sigmoid(self.propagate_layer_forward(self.weights_4, self.biases_4, self.ReLU(self.layer_3)))
         return output
         
+    def binary_cross_entropy(self, prediction, truth):
+        return -np.mean(truth * np.log(prediction) + (1 - truth) * np.log(1 - prediction))
+    
+    def back_prop(self):
+        pass
+
+    def binary_cross_entropy_gradient(self, prediction, truth):
+        return -((truth/prediction)-(1-truth)/(1-prediction))
+    
+    def sigmoid_gradient(self, x):
+        return self.sigmoid(x) * (1-self.sigmoid(x))
+    
+
 
     def test_net(self, test_input):
         self.output = self.propagate_network_forward(test_input)
         print(self.output)
         return self.output
         
-        
-        
-
         
 if __name__ == "__main__":
     test_input = np.zeros((1, 24))
